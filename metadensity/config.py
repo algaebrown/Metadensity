9569586fe23cda48da355d5644ea754099506b5c
Hsuan-lin Her <hsher@tscc-login1.sdsc.edu>
2020-09-28 14:26:09 -0700

    fix relative improt err

---
+++ metadensity/config.py
@@ -0,0 +1,12 @@
+# set up genome coordinates
+
+
+# change function
+class MetadensityConfig:
+
+    def __init__(self):
+        self.root_dir = '/home/hsher/seqdata/20200622_gencode_coords_hsher/'
+        self.transcript_fname = 'gencode.v33.transcript.gff3'
+        self.gencode_feature_fname = 'gencode.v33.combine.sorted.gff3'
+
+settings = MetadensityConfig()
