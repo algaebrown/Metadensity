39b4442b49470523719ee5c85227bfe01a8f41d7
Hsuan-lin Her <hsher@tscc-login1.sdsc.edu>
2020-10-15 10:46:15 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = "Metadensity - Metagene plot using CLIP-seq and STAMP, for RNAs"
+
+setup(
+    name = "metadensity",
+    long_description = long_description,
+    version = "0.0.1",
+    packages = find_packages(),
+    
+
+    package_data = {
+        'metadensity' : ['data/*']
+        },
+
+    install_requires = ['setuptools', 
+                        'pysam >= 0.15.4',
+                        'numpy >= 1.18.1 ',
+                        'scipy >= 1.4.1',
+                        'matplotlib >= 3.1.3',
+                        'pybedtools >= 0.8.1',
+                        'pandas >= 1.0.2',
+                        'pybigwig >= 0.3.17',
+                        'seaborn >= 0.10.0',
+                        'biopython >= 1.77',
+                        'scikit-learn >= 0.22.1'
+                        ],
+      
+    setup_requires = ["setuptools_git >= 0.3",],
+    
+    #scripts=['scripts/region_call.py', 'scripts/shrink_region.py'],
+     
+
+    #metadata for upload to PyPI
+    author = "Hsuan-lin Her",
+    author_email = "hsher@ucsd.edu",
+    description = "A set of scripts visualizing eCLIP data",
+    license = "GPL2",
+    keywords = "CLIP-seq, metagene, bioinformatics",
+    url = "https://github.com/algaebrown/Metadensity",
+    
+    #Other stuff I feel like including here
+    #include_package_data = True
+    #zip_safe = True #True I think
+)
